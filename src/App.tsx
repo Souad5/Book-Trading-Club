6a737b4347c76660fcd0c0a58a508fd27f5928aa
Amirun Nahar <99985523+Naharamina@users.noreply.github.com>
2025-09-17 23:13:58 +0600

    added building related file

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Home from './pages/Home'
+import Browse from './pages/Browse'
+import HowItWorks from './pages/HowItWorks'
+
+function App() {
+  return (
+    <div className="min-h-full flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/browse" element={<Browse />} />
+          <Route path="/how-it-works" element={<HowItWorks />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
