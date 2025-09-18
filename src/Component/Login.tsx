086d8bb941517199a7fbd4e4436950d947810d59
hridoy8220 <140155955+hridoy8220@users.noreply.github.com>
2025-09-19 03:31:27 +0600

    add password eye button

--- src/Component/Login.tsx
+++ src/Component/Login.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 import { FaFacebookF, FaApple } from "react-icons/fa";
-import picture from '../assests/photo/login-image.png'
-
+import picture from "../assests/photo/login-image.png";
+import { Link } from "react-router";
 const Login = () => {
-    return (
-            <div className="min-h-screen flex items-center justify-center bg-gray-50">
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="flex flex-col lg:flex-row bg-white rounded-xl shadow-lg overflow-hidden max-w-4xl w-full">
-        
         {/* Left Form Section */}
         <div className="w-full lg:w-1/2 p-8 md:p-16 flex flex-col justify-center">
           <div className="mb-8">
@@ -27,13 +29,20 @@ const Login = () => {
                 className="w-full mt-1 p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
-            <div>
+            <div className="relative">
               <label className="text-gray-600 text-sm">Password</label>
               <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 placeholder="••••••••••••"
                 className="w-full mt-1 p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute mt-3 right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-blue-500 transition-colors"
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
             </div>
 
             <div className="flex justify-between items-center text-sm text-gray-500">
@@ -41,7 +50,9 @@ const Login = () => {
                 <input type="checkbox" className="mr-2" />
                 Remember me
               </label>
-              <a href="#" className="text-red-500">Forgot Password?</a>
+              <a href="#" className="text-red-500">
+                Forgot Password?
+              </a>
             </div>
 
             <button
@@ -54,7 +65,11 @@ const Login = () => {
 
           <p className="text-center text-gray-500 text-sm mt-4">
             Don’t have an account?{" "}
-            <a href="#" className="text-red-500 font-semibold">Sign up</a>
+            <Link to={"/register"}>
+              <a href="#" className="text-red-500 font-semibold">
+                Sign up
+              </a>
+            </Link>
           </p>
 
           <div className="mt-6 text-center text-gray-400 text-sm relative">
@@ -85,7 +100,7 @@ const Login = () => {
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Login;
