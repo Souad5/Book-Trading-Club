d095ed18df0a2d39162d218a1e59b8991216b819
Souad5 <souadalkabir@gmail.com>
2025-09-19 16:46:28 +0600

    admin-dashboard added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Home from './pages/Home'
 import Browse from './pages/Browse'
 import HowItWorks from './pages/HowItWorks'
+import About from './pages/About'
+import AdminDashboard from './dashboard/AdminDashboard'
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/browse" element={<Browse />} />
           <Route path="/how-it-works" element={<HowItWorks />} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/admin-dashboard" element={<AdminDashboard/>} />
         </Routes>
       </main>
       <Footer />
