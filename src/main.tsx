2b5f6e845195c9f6f0cc0ac8d4d8855d80971e19
Souad5 <souadalkabir@gmail.com>
2025-09-25 14:49:31 +0600

    deploy vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
-import './index.css';
-import { ToastContainer } from 'react-toastify';
-import { AuthProvider } from './firebase/AuthProvider'; // <-- import AuthProvider
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import './app.css'
+import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "./firebase/AuthProvider"; // <-- import AuthProvider
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
@@ -25,5 +25,5 @@ createRoot(document.getElementById('root')!).render(
         />
       </AuthProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
