1fdf1af2c0fbea925a7ce5f61f33e06ac6fe1326
Amirun Nahar <99985523+Naharamina@users.noreply.github.com>
2025-10-16 21:21:53 +0600

    Modified favorite books page

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import Login from './Component/Login';
 import Dashboard from './dashboard/DashBoard';
 import AdminDashboard from './dashboard/Admin/AdminDashboard';
 import AddNewBook from './components/Section/AddNewBook';
+<<<<<<< Updated upstream
 =======
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
@@ -28,11 +29,27 @@ import AdminDashboard from './dashboard/Admin/AdminDashboard'
 import AddNewBook from './components/Section/AddNewBook'
 import FavouriteBooks from './pages/Wishlist'
 >>>>>>> Stashed changes
+=======
+import FavouriteBooks from './pages/Wishlist';
+
+import { useAuth } from './firebase/AuthProvider';
+import Loader from './components/SharedComponents/Loader';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import ChatPopup from './components/Modals/ChatPopup';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="min-h-full flex flex-col">
       <Navbar />
+<<<<<<< Updated upstream
+=======
+      <ChatPopup></ChatPopup>
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop closeOnClick pauseOnFocusLoss draggable pauseOnHover theme="light" />
+>>>>>>> Stashed changes
       <main className="flex-1">
         <Routes>
           <Route path="/" element={<Home />} />
