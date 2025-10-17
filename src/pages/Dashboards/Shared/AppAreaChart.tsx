3f7d3ba658281500f9b9a423d93dd4959451083e
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-10-17 16:10:49 +0600

    Charts Added

---
+++ src/pages/Dashboards/Shared/AppAreaChart.tsx
@@ -0,0 +1,77 @@
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart';
+
+import { chartConfig, chartData } from './AppBarChart';
+import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
+
+const AppAreaChart = () => {
+  return (
+    <ChartContainer config={chartConfig}>
+      <AreaChart
+        accessibilityLayer
+        data={chartData}
+        margin={{
+          left: 12,
+          right: 12,
+        }}
+      >
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey="month"
+          tickLine={false}
+          axisLine={false}
+          tickMargin={8}
+          tickFormatter={(value) => value.slice(0, 3)}
+        />
+        <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+        <defs>
+          <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+            <stop
+              offset="5%"
+              stopColor="var(--color-desktop)"
+              stopOpacity={0.8}
+            />
+            <stop
+              offset="95%"
+              stopColor="var(--color-desktop)"
+              stopOpacity={0.1}
+            />
+          </linearGradient>
+          <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+            <stop
+              offset="5%"
+              stopColor="var(--color-mobile)"
+              stopOpacity={0.8}
+            />
+            <stop
+              offset="95%"
+              stopColor="var(--color-mobile)"
+              stopOpacity={0.1}
+            />
+          </linearGradient>
+        </defs>
+        <Area
+          dataKey="mobile"
+          type="natural"
+          fill="url(#fillMobile)"
+          fillOpacity={0.4}
+          stroke="var(--color-mobile)"
+          stackId="a"
+        />
+        <Area
+          dataKey="desktop"
+          type="natural"
+          fill="url(#fillDesktop)"
+          fillOpacity={0.4}
+          stroke="var(--color-desktop)"
+          stackId="a"
+        />
+      </AreaChart>
+    </ChartContainer>
+  );
+};
+
+export default AppAreaChart;
--- src/pages/Dashboards/Shared/DashboardHomePage.tsx
+++ src/pages/Dashboards/Shared/DashboardHomePage.tsx
@@ -1,7 +1,21 @@
+import AppAreaChart from './AppAreaChart';
+import AppBarChart from './AppBarChart';
+
 const DashboardHomePage = () => {
   return (
-    <div>
-      <h1>This is the Dashboard Home Page</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2">
+        <h1 className="text-lg font-medium mb-6">Total Users</h1>
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2">
+        <h1 className="text-lg font-medium mb-6">Total Users</h1>
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">Test</div>
     </div>
   );
 };
