bd4cbfb331815018fae20a5e9f644525a4504fdd
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-10-17 15:19:32 +0600

    Sidebar Completed

--- src/pages/Wishlist/Wishlist.tsx
+++ src/pages/Wishlist/Wishlist.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Heart, Search } from 'lucide-react';
 import { toast } from 'react-toastify';
