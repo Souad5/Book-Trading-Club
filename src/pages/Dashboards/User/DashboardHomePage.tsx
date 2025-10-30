7a342f68f28e5577cbfb887165634b6c1b61a6b9
Amirun Nahar <99985523+Naharamina@users.noreply.github.com>
2025-10-30 19:51:23 +0600

    Added

---
+++ src/pages/Dashboards/User/DashboardHomePage.tsx
@@ -0,0 +1,32 @@
+import AppBarChart from "../Shared/AppBarChart";
+import CardList from "../Shared/CardList";
+import AppPieChart from "../Shared/AppPieChart";
+import AppAreaChart from "../Shared/AppAreaChart";
+
+const DashboardHomePage = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2">
+        <h1 className="text-lg font-medium mb-6">Total Users</h1>
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">
+        <CardList title="Latest Transactions"></CardList>
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">
+        <h1 className="text-lg font-medium mb-6">Browser Usage</h1>
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2">
+        <h1 className="text-lg font-medium mb-6">Total Users</h1>
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg ">
+        <CardList title="Popular Content"></CardList>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardHomePage;
